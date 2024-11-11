806a5c800234adcc7a0391191fd1396cc0635676
이진욱KR <apple_komputer@icloud.com>
2024-11-11 13:33:14 +0900

    첫커밋 : 프로젝트생성

---
+++ 3W-CalculatorApp/3W-CalculatorApp/Controllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  3W-CalculatorApp
+//
+//  Created by 진욱의 Macintosh on 11/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ 3W-CalculatorApp/3W-CalculatorApp/Views/CalculatorView.swift
@@ -0,0 +1,14 @@
+//
+//  CalculatorView.swift
+//  3W-CalculatorApp
+//
+//  Created by 진욱의 Macintosh on 11/11/24.
+//
+
+import UIKit
+
+class CalculatorView: UIView {
+
+  
+
+}
